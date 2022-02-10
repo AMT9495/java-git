bb1b69a9a0fc2eea556ed6f96a6d8729b2db2236
AungMyoThant9 <P_Boom@192.168.100.9>
2022-02-11 02:04:38 +0630

    "test"

--- java-basic/src/day_2/testing2.java
+++ java-basic/src/day_2/testing2.java
@@ -4,7 +4,9 @@ public class testing2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello");
+		System.out.println("hi");
+		System.out.println("gg");
 	}
 
 }
